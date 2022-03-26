5899e37798d869f4502dfe723ac4d4d83b9c840a
Francesco Ratti <francesco2.ratti@mail.polimi.it>
2022-03-26 16:17:12 +0100

    improvements in login

--- src/main/java/com/polimi/mrf/appartapp/resources/LoginResource.java
+++ src/main/java/com/polimi/mrf/appartapp/resources/LoginResource.java
@@ -6,31 +6,33 @@ import com.polimi.mrf.appartapp.entities.User;
 
 import javax.ejb.EJB;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/login")
 public class LoginResource {
+
     @EJB(name = "com.polimi.mrf.appartapp.beans/UserServiceBean")
     UserServiceBean userServiceBean;
 
     @POST
     @Produces("application/json")
     public Response login(@Context HttpServletRequest request) {
+
         String email=request.getParameter("email");
         String password=request.getParameter("password");
 
         if (email==null || password==null)
-            return Response.status(Response.Status.UNAUTHORIZED).build();
+            return Response.status(Response.Status.BAD_REQUEST).type(MediaType.TEXT_PLAIN).entity("missing parameters").build();
 
         User user=userServiceBean.getUser(email, password);
         if (user==null)
-            return Response.status(Response.Status.UNAUTHORIZED).build();
+            return Response.status(Response.Status.UNAUTHORIZED).type(MediaType.TEXT_PLAIN).entity("unauthorized").build();
         else
-            return Response.status(Response.Status.OK).entity(new Gson().toJson(user)).build();
+            return Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON).entity(new Gson().toJson(user)).build();
     }
 }
