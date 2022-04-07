3928338baea936678e8307ff92c3f6f330ab7760
Francesco Ratti <francesco2.ratti@mail.polimi.it>
2022-04-07 17:00:35 +0200

    added UserManager filter, which retrieves user object in "private" area and appends User object to request

---
+++ src/main/java/com/polimi/mrf/appartapp/filter/UserManager.java
@@ -0,0 +1,38 @@
+package com.polimi.mrf.appartapp.filter;
+
+import com.polimi.mrf.appartapp.beans.UserServiceBean;
+import com.polimi.mrf.appartapp.entities.User;
+
+import javax.ejb.EJB;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+
+public class UserManager extends HttpFilter {
+
+    @EJB(name = "com.polimi.mrf.appartapp.beans/UserServiceBean")
+    UserServiceBean userServiceBean;
+
+    @Override
+    public void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException, ServletException {
+        String email=req.getParameter("email");
+        String password=req.getParameter("password");
+
+        if (email==null || password==null || email.length()==0 || password.length()==0) {
+            res.setStatus(Response.Status.UNAUTHORIZED.getStatusCode());
+            return;
+        }
+
+        User user=userServiceBean.getUser(email, password);
+        if (user==null) {
+            res.setStatus(Response.Status.UNAUTHORIZED.getStatusCode());
+        } else {
+            req.setAttribute("user", user);
+            chain.doFilter(req, res);
+        }
+    }
+}
