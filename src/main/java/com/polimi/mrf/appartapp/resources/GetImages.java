044817131617a374f492b61721f29c317df62f18
Francesco Ratti <francesco2.ratti@mail.polimi.it>
2022-04-14 13:19:20 +0200

    added getImages for user images w/ security checks

--- src/main/java/com/polimi/mrf/appartapp/resources/GetImages.java
+++ src/main/java/com/polimi/mrf/appartapp/resources/GetImages.java
@@ -1,23 +1,45 @@
 package com.polimi.mrf.appartapp.resources;
 
 import com.polimi.mrf.appartapp.beans.ApartmentServiceBean;
+import com.polimi.mrf.appartapp.beans.UserServiceBean;
+import com.polimi.mrf.appartapp.entities.User;
+import com.polimi.mrf.appartapp.entities.UserImage;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.File;
 
-@Path("/images")
 public class GetImages {
     @GET
-    @Path("apartments/{id}")
+    @Path("/images/apartments/{id}")
     @Produces("image/jpg")
     public Response getApartmentImage(@PathParam("id") String idStr) {
         return buildResponse(idStr, ApartmentServiceBean.apartmentImagesFolderPath);
     }
+
+    @GET
+    @Path("/reserved/images/users/{id}")
+    @Produces("image/jpg")
+    public Response getUserImage(@PathParam("id") String idStr, @Context HttpServletRequest request) {
+        User user= (User) request.getAttribute("user");
+        try {
+            //avoiding useless lookup , since equals method of Image class has been overridden, returns true if ids are the same
+            UserImage userImage = new UserImage();
+            userImage.setId(Long.parseLong(idStr));
+            if (user.getImages().contains(userImage)) //security check TODO: allow matches too! not only logged user to see his/her pictures
+                return buildResponse(idStr, UserServiceBean.userImagesFolderPath);
+        } catch (NumberFormatException e) {
+            return Response.status(Response.Status.NOT_ACCEPTABLE).type(MediaType.TEXT_PLAIN).entity("id not numeric").build();
+        }
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.TEXT_PLAIN).entity("INTERNAL_SERVER_ERROR").build();
+    }
+
     public Response buildResponse(String idStr, String folderPath) {
         if (idStr == null || idStr.length() == 0)
             return Response.status(Response.Status.BAD_REQUEST).type(MediaType.TEXT_PLAIN).entity("missing parameters").build();
